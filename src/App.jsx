802a606b199e9d066ae1d2da6f3ca0440b06575c
Carl <carlmathewgabay123@gmail.com>
2024-10-25 13:46:24 +0800

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 let themeColor = (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
-document.body.styleSheet = "@apply", themeColor == "dark" ? "bg-slate-950" : "bg-gray-400;";
+const darkColors = [
+	"bg-slate-950",
+];
+const lightColors = [
+	"bg-gray-400",
+];
+
+function changeTheme() {
+	const elements = document.querySelectorAll("*");
+
+	elements.forEach(i => {
+		darkColors.forEach((c, ii) => {
+			if(i.classList.contains(c) || i.classList.contains(lightColors[ii])) {
+				i.classList.remove(darkColors[ii], lightColors[ii]);
+				i.classList.add(themeColor == "dark" ? darkColors[ii] : lightColors[ii]);
+			}
+		});
+	});
+}
+changeTheme();
 
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
 	themeColor = event.matches ? "dark" : "light";
-
-	console.log(themeColor == "dark" ? "bg-slate-950" : "bg-gray-400");
-	document.body.styleSheet = themeColor == "dark" ? "bg-slate-950" : "bg-gray-400;";
-	console.log("@apply " + themeColor == "dark" ? "bg-slate-950" : "bg-gray-400")
+	changeTheme();
 });
 
 const App = () => {
