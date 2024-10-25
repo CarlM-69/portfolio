a5b172381735bc986bfae1b7adaa10220db0a943
Carl <carlmathewgabay123@gmail.com>
2024-10-25 14:42:42 +0800

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-let themeColor = (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
-const darkColors = [
-	"bg-slate-950",
-];
-const lightColors = [
-	"bg-gray-400",
-];
-
-function changeTheme() {
-	const elements = document.querySelectorAll("*");
-
-	elements.forEach(i => {
-		darkColors.forEach((c, ii) => {
-			if(i.classList.contains(c) || i.classList.contains(lightColors[ii])) {
-				i.classList.remove(darkColors[ii], lightColors[ii]);
-				i.classList.add(themeColor == "dark" ? darkColors[ii] : lightColors[ii]);
-			}
-		});
-	});
-}
-changeTheme();
-
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
-	themeColor = event.matches ? "dark" : "light";
-	changeTheme();
-});
+import { useEffect, useState } from "react";
 
 const App = () => {
+	const [theme, setTheme] = useState("dark");
+
+	useEffect(() => {
+		document.documentElement.classList.toggle("dark", theme == "dark");
+	}, [theme]);
+
+	window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
+		setTheme(event.matches ? "dark" : "light");
+	});
+
 	return (
-		<div className="bg_background_bg bg-gray-400">
+		<div className="bg_background_bg bg-gray-200">
 
 		</div>
 	);
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-window.onload = ()=> {
+document.body.classList.add("bg-gray-200", "dark:bg-slate-950", "transition-colors", "duration-500");
+window.onload = () => {
 	createRoot(document.getElementById('root')).render(
 		<StrictMode>
 			<App />
