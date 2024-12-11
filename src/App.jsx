50db1d982266427c2601ba86a4dff487c1434eb2
Carl <carlmathewgabay123@gmail.com>
2024-12-11 22:40:04 +0800

    v12.11.24

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ const App = () => {
 				<div className="w-full h-full">
 					<span className="text-3xl md:text-5xl font-sourceCP text-stone-900 dark:text-gray-200 relative top-1/2 md:top-[45%] left-[7%] cursor-default select-none">
 						Welcome.
-						<span className="description block text-xl md:text-2xl w-4/5 md:w-2/5 text-stone-700 dark:text-stone-400"></span>
+						<span className="description block text-xl md:text-2xl w-[16rem] md:w-[25rem] text-stone-700 dark:text-stone-400"></span>
 					</span>
 				</div>
 			</div>
