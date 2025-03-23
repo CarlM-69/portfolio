db60c96aae2c8c865a29f169e6568ec1bd7f0432
CarlM-69 <carlmathewgabay123@gmail.com>
2025-03-23 18:12:29 +0800

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Invite from "./pages/Invite/Invite.jsx";
 
 export default function App() {
 	return (
-		<>
-		
-		</>
+		<Routes>
+			<Route path="/Welcome/" element={ <Welcome /> } />
+			<Route path="/Invite/" element={ <Invite /> } />
+
+			<Route path="/" element={ <Navigate to="/Welcome/" replace /> } />
+		</Routes>
 	)
 };
