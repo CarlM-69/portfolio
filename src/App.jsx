72ed8305811caf161a1d7f5059edef138344d8fb
CarlM-69 <carlmathewgabay123@gmail.com>
2025-03-23 14:46:07 +0800

    fix rerouting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Welcome from "./pages/Welcome/Index.jsx";
 import Invite from "./pages/Invite/Index.jsx";
 
 export default function App() {
 	return (
-		<HashRouter>
-			<Routes>
-				<Route path="/Welcome/" element={ <Welcome /> } />
-				<Route path="/Invite/" element={ <Invite /> } />
+		<Routes>
+			<Route path="/Welcome/" element={ <Welcome /> } />
+			<Route path="/Invite/" element={ <Invite /> } />
 
-				<Route path="/" element={ <Navigate to="/Welcome/" replace /> } />
-			</Routes>
-		</HashRouter>
+			<Route path="/" element={ <Navigate to="/Welcome/" replace /> } />
+		</Routes>
 	)
 };
