7d3875625e33d8c5612e17deda59780a82b11007
CarlM-69 <carlmathewgabay123@gmail.com>
2025-03-23 14:39:21 +0800

    try fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Welcome from "./pages/Welcome/Index.jsx";
 import Invite from "./pages/Invite/Index.jsx";
 
 export default function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path="/Welcome/" element={ <Welcome /> } />
 				<Route path="/Invite/" element={ <Invite /> } />
 
 				<Route path="/" element={ <Navigate to="/Welcome/" replace /> } />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	)
 };
