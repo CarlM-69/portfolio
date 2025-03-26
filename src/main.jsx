9259e03ba1fd13bcecaf15b3afd7ff08107baad0
CarlM-69 <carlmathewgabay123@gmail.com>
2025-03-26 18:03:50 +0800

    placeholder

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 import { HashRouter } from 'react-router-dom';
 
 window.onload = () => {
-  createRoot(document.getElementById('root')).render(
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
+	createRoot(document.getElementById('root')).render(
+		<HashRouter>
+			<App />
+		</HashRouter>
+	);
 };
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,7 +1,9 @@
+import "./Welcome.css";
+
 export default function Welcome() {
 	return (
 		<>
-		
+			<div className="something"><span>not yet done. hehehe</span></div>
 		</>
 	)
 };
