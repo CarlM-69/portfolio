c3787db0805dcb651ca9c48947cb88de8eaae343
CarlM-69 <carlmathewgabay123@gmail.com>
2025-03-26 18:44:01 +0800

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 window.onload = () => {
 	createRoot(document.getElementById('root')).render(
-		<HashRouter>
+		<BrowserRouter>
 			<App />
-		</HashRouter>
+		</BrowserRouter>
 	);
 };
