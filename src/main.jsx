127dfe33e01721a007fa591a801394c63daf8375
CarlM-69 <carlmathewgabay123@gmail.com>
2025-03-26 19:52:31 +0800

    revert

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 window.onload = () => {
 	createRoot(document.getElementById('root')).render(
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
