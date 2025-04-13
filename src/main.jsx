5a07e6597d03a85485154d03673c40d5e61e28e9
Carl Mathew Gabay <carlmathewgabay123@gmail.com>
2025-04-13 18:40:14 +0800

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
 import { HashRouter } from 'react-router-dom';
 
 window.onload = () => {
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -3,8 +3,9 @@ import "./Welcome.css";
 export default function Welcome() {
 	return (
 		<>
+			<div className="bg"><div className="bg-gradient" /></div>
 			<div className="nav">
-				
+
 			</div>
 		</>
 	)
