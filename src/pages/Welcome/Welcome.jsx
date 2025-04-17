7624cd70d684bc027aa187791ebf625ef7c85946
CarlM-69 <carlmathewgabay123@gmail.com>
2025-04-18 00:00:48 +0800

    landing changes

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,3 +1,4 @@
+import "../../css/global.css";
 import "./Welcome.css";
 
 export default function Welcome() {
@@ -7,7 +8,7 @@ export default function Welcome() {
 				<span className="main-title">MY PORTFOLIO</span>
 				<span className="main-subtitle" id="subtitle">I'm Carl Mathew Gabay.</span>
 				<div className="down-arrow">
-					<img src="/down-arrow.svg" alt="arrow down indicator" draggable="false" />
+					<img src="/svg/down-arrow.svg" alt="arrow down indicator" draggable="false" />
 				</div>
 			</div>
 			<div className="container">
