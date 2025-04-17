4fca33e37c99b2dc87bec81b5e7ffc6bf241941e
CarlM-69 <carlmathewgabay123@gmail.com>
2025-04-18 00:33:00 +0800

    res changes

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,13 +1,48 @@
 import "../../css/global.css";
 import "./Welcome.css";
 
+import { useEffect, useState } from "react";
+
 export default function Welcome() {
+	const [device, setDevice] = useState("");
+
+	useEffect(() => {
+		const redirect_2ndpage = document.querySelector("#redirect-to-2nd-page");
+		const redirect_2ndpage_dest = document.querySelector("#asd");
+
+		redirect_2ndpage.addEventListener("click", () => {
+			redirect_2ndpage_dest.scrollIntoView({
+				behavior: "smooth",
+				block: "start"
+			});
+		});
+
+		window.addEventListener("scroll", (e) => {
+			let scroll = window.scrollY;
+			console.log(scroll);
+		});
+
+		window.addEventListener("resize", (e) => {
+			let width = window.innerWidth;
+			// console.log(width, device);
+
+			if(width < 768) setDevice("mobile");
+			else if(width < 1024) setDevice("tablet");
+			else if(width >= 1280) setDevice("desktop");
+		});
+	}, []);
+
+	useEffect(() => {
+		let width = window.innerWidth;
+		console.log(width, device);
+	}, [device]);
+
 	return (
 		<>
 			<div className="container">
 				<span className="main-title">MY PORTFOLIO</span>
 				<span className="main-subtitle" id="subtitle">I'm Carl Mathew Gabay.</span>
-				<div className="down-arrow">
+				<div className="down-arrow" id="redirect-to-2nd-page">
 					<img src="/svg/down-arrow.svg" alt="arrow down indicator" draggable="false" />
 				</div>
 			</div>
@@ -15,7 +50,7 @@ export default function Welcome() {
 
 			</div>
 			<div className="container">
-				
+				<span id="asd">asd</span>
 			</div>
 		</>
 	)
