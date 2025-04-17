bfe84e380930cd23992b8a0299e823558179526e
CarlM-69 <carlmathewgabay123@gmail.com>
2025-04-17 14:30:15 +0800

    homepage changes

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -3,10 +3,18 @@ import "./Welcome.css";
 export default function Welcome() {
 	return (
 		<>
-			<div className="bg"><div className="bg-gradient" /></div>
-			<div className="nav">
+			<div className="container">
+				<span className="main-title">MY PORTFOLIO</span>
+				<span className="main-subtitle" id="subtitle">I'm Carl Mathew Gabay. </span>
+				
+			</div>
+			<div className="container">
 
 			</div>
+			<div className="container">
+				
+			</div>
+			<div className="bg"><div className="bg-gradient" /></div>
 		</>
 	)
 };
