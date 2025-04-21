81128e8fcc3dc528c3e839fbe50ad702b0502e11
CarlM-69 <carlmathewgabay123@gmail.com>
2025-04-21 23:00:33 +0800

    3rd page setup

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -2,6 +2,7 @@ import "../../css/global.css";
 import "./Welcome.css";
 
 import { useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Welcome() {
 	const [device, setDevice] = useState("");
@@ -43,16 +44,21 @@ export default function Welcome() {
 			<div className="container">
 				<div className="box-wrapper">
 					<div className="box">
-						<div className="item"></div>
-						<div className="item"></div>
-						<div className="item"></div>
-						<div className="item"></div>
-						<div className="item"></div>
+
 					</div>
 				</div>
 			</div>
 			<div className="container">
-				<span id="asd">asd</span>
+				<span className="message">Thank you for taking the time to check out my portfolio! I hugely appreciate your consideration to invite me and contribute my skills on your project. I can say that I'm confident enough to bring value and quality to the work and looking forward to be one of the member of your team and the production!</span>
+				<div className="social-media">
+					<img src="/svg/social/discord.svg" alt="discord" draggable="false" className="media" />
+					<a href="https://www.facebook.com/carlmathewgabay" target="_blank" className="media"><img src="/svg/social/facebook.svg" alt="facebook" draggable="false" /></a>
+					<a href="https://github.com/CarlM-69" target="_blank" className="media"><img src="/svg/social/github.svg" alt="github" draggable="false" /></a>
+					<a href="https://www.instagram.com/crl.carl.crl.carl.crl.carl/" target="_blank" className="media"><img src="/svg/social/instagram.svg" alt="instagram" draggable="false" /></a>
+					<a href="https://www.linkedin.com/in/carlmathewgabay/" target="_blank" className="media"><img src="/svg/social/linkedin.svg" alt="linkedin" draggable="false" /></a>
+					<a href="https://stackoverflow.com/users/22312537/carlll" target="_blank" className="media"><img src="/svg/social/stackoverflow.svg" alt="stackoverflow" draggable="false" /></a>
+					<a href="https://wa.me/09350061735" target="_blank" className="media"><img src="/svg/social/whatsapp.svg" alt="whatsapp" draggable="false" /></a>
+				</div>
 			</div>
 			<div className="container">
 				<span id="asd">asd</span>
