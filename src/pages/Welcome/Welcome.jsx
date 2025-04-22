b0636bdf82a66483cfb065bd08c1c59cd601b83d
CarlM-69 <carlmathewgabay123@gmail.com>
2025-04-22 12:24:43 +0800

    discord pop up changes

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -6,11 +6,18 @@ import emailjs from "@emailjs/browser";
 
 export default function Welcome() {
 	const [device, setDevice] = useState("");
+	var social_discord_mouseover;
+	var social_discord_mouseout;
+	var social_discord_click;
 
 	useEffect(() => {
+		let width = window.innerWidth;
+		if(width < 768) setDevice("mobile");
+		else if(width < 1024) setDevice("tablet");
+		else if(width >= 1280) setDevice("desktop");
+
 		const redirect_2ndpage = document.querySelector("#redirect-to-2nd-page");
 		const redirect_2ndpage_dest = document.querySelector("#asd");
-		const social_discord = document.querySelector(".media-pop_up");
 
 		redirect_2ndpage.addEventListener("click", () => {
 			redirect_2ndpage_dest.scrollIntoView({
@@ -31,9 +38,35 @@ export default function Welcome() {
 			else if(width < 1024) setDevice("tablet");
 			else if(width >= 1280) setDevice("desktop");
 		});
-
-		
 	}, []);
+
+	useEffect(() => {
+		console.log(device);
+		const social_discord = document.querySelector(".media-pop_up");
+		const social_discord_box = document.querySelector(".pop-up");
+		let allow_copy = false;
+
+		removeEventListener("mouseover", social_discord_mouseover);
+		removeEventListener("mouseout", social_discord_mouseout);
+		removeEventListener("click", social_discord_click);
+
+		social_discord_mouseover = social_discord.addEventListener((device == "desktop" ? "mouseover" : "click"), () => {
+			social_discord_box.querySelector("span:nth-child(2)").innerText = (device == "desktop" ? "Double click to copy." : "Click to copy.");
+			social_discord_box.classList.add("show");
+			allow_copy = true;
+		});
+
+		social_discord_mouseout = social_discord.addEventListener("mouseout", () => {
+			social_discord_box.classList.remove("show");
+			allow_copy = false;
+		});
+
+		social_discord_click = social_discord.addEventListener((device == "desktop" ? "dblclick" : "click"), () => {
+			if(!allow_copy) return; 
+			navigator.clipboard.writeText("carl.hahah");
+			social_discord_box.querySelector("span:nth-child(2)").innerText = "Copied!";
+		});
+	}, [device]);
 
 	return (
 		<>
@@ -56,8 +89,8 @@ export default function Welcome() {
 				<div className="social-media">
 					<div className="media">
 						<div className="pop-up">
-							<span>Tag: carl.hahah</span>
-							<span>ID: 308940153001476097</span>
+							<span>TAG: carl.hahah</span>
+							<span>{ device == "desktop" ? "Double click to copy." : "Click to copy." }</span>
 						</div>
 						<img src="/svg/social/discord.svg" alt="discord" draggable="false" className="media-pop_up" />
 					</div>
@@ -66,7 +99,7 @@ export default function Welcome() {
 					<a href="https://www.instagram.com/crl.carl.crl.carl.crl.carl/" target="_blank" className="media"><img src="/svg/social/instagram.svg" alt="instagram" draggable="false" /></a>
 					<a href="https://www.linkedin.com/in/carlmathewgabay/" target="_blank" className="media"><img src="/svg/social/linkedin.svg" alt="linkedin" draggable="false" /></a>
 					<a href="https://stackoverflow.com/users/22312537/carlll" target="_blank" className="media"><img src="/svg/social/stackoverflow.svg" alt="stackoverflow" draggable="false" /></a>
-					<a href="https://wa.me/09350061735" target="_blank" className="media"><img src="/svg/social/whatsapp.svg" alt="whatsapp" draggable="false" /></a>
+					<a href="https://wa.me/639350061735" target="_blank" className="media"><img src="/svg/social/whatsapp.svg" alt="whatsapp" draggable="false" /></a>
 				</div>
 			</div>
 			<div className="container">
