d46f4836eefa0ab7e56879624c200bc6978f4993
Carl Mathew <carlmathewgabay123@gmail.com>
2025-07-09 01:06:37 +0800

    refactor: restructure Welcome component layout with child divs for better organization

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -200,12 +200,19 @@ export default function Welcome() {
 	return (
 		<>
 			<div className="container">
+				<div className="child"><span className="main-title">MY PORTFOLIO</span></div>
+				<div className="child"><span className="main-subtitle" id="subtitle">I'm Carl Mathew Gabay.</span></div>
+				<div className="child">
+					<img id="redirect-to-2nd-page" src="/svg/down-arrow.svg" alt="down arrow indicator" draggable="false" />
+				</div>
+			</div>
+			{/* <div className="container">
 				<span className="main-title">MY PORTFOLIO</span>
 				<span className="main-subtitle" id="subtitle">I'm Carl Mathew Gabay.</span>
 				<div className="down-arrow" id="redirect-to-2nd-page">
 					<img src="/svg/down-arrow.svg" alt="arrow down indicator" draggable="false" />
 				</div>
-			</div>
+			</div> */}
 			<div className="container" id="preview-container">
 				<div className="box-wrapper">
 					<div className="box">
